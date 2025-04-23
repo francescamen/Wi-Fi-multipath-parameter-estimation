b1b81c41074978336b2a01abf3ccbe841e844da8
Francesca Meneghello <23723290+francescamen@users.noreply.github.com>
2025-04-23 22:04:17 +0200

    Add file Grid_ToA_AoA_AoD_smoothing.m

--- Matlab_code/functions/Grid_ToA_AoA_AoD_smoothing.m
+++ Matlab_code/functions/Grid_ToA_AoA_AoD_smoothing.m
@@ -1,15 +1,3 @@
-%     Copyright (C) 2023 Alejandro Blanco
-%     This program is free software: you can redistribute it and/or modify
-%     it under the terms of the GNU General Public License as published by
-%     the Free Software Foundation, either version 3 of the License, or
-%     (at your option) any later version.
-%     This program is distributed in the hope that it will be useful,
-%     but WITHOUT ANY WARRANTY; without even the implied warranty of
-%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%     GNU General Public License for more details.
-%     You should have received a copy of the GNU General Public License
-%     along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 function [S_toa, S_aoa, S_aod, index_time, theta_aoa, theta_aod] = Grid_ToA_AoA_AoD_smoothing(step_toa, step_aoa, step_aod, K, N, M, BW, N_toa)
 %UNTITLED9 Summary of this function goes here
 %   Detailed explanation goes here
